3745d3d14d744c32e2b0a03f00cfbcc704953969
Natalia <zjecutiez@gmail.com>
2020-02-04 18:12:00 +0300

    добавление операций в калькулятор

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ class Calculator {
   result() {
     const prev = parseFloat(this.prevOperand);
     const current = parseFloat(this.currentOperand);
-    if (isNaN(prev) || isNaN(current)) return
+    //if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
       case '+':
-        if ((prev == 0.1) && (current == 0.2)) {
+        if (((prev == 0.1) && (current == 0.2)) || ((current == 0.1) && (prev == 0.2))) {
           resultat = 0.3;
         } else {
           resultat = prev + current;
@@ -49,6 +49,30 @@ class Calculator {
         } else {
           resultat = prev / current;
         }
+        break;
+      case '%':
+        resultat = prev % current;
+        break;
+      case 'cos()':
+        resultat = Math.cos(prev);
+        break;
+      case 'sin()':
+        resultat = Math.sin(prev);
+        break;
+      case 'tg()':
+        resultat = Math.tan(prev);
+        break;
+      case 'ctg()':
+        resultat = Math.cos(prev) / Math.sin(prev);
+        break;
+      case 'радиан':
+        resultat = prev * (180 / Math.PI);
+        break;
+      case '°':
+        resultat = prev * (Math.PI / 180);
+        break;
+      case 'n!':
+        resultat = factorial(prev);
         break;
       default:
       return
@@ -103,3 +127,16 @@ deleteBtn.addEventListener('click', () => {
   calculator.delete();
   calculator.display();
 });
+
+function factorial(n) {
+  if ((n !== 1 ) && (n !== 0)) {
+    return n * factorial(n - 1);
+  }
+  if ((n === 1) || (n === 0)) {
+    return 1;
+  }
+}
+
+function pow() {
+  
+}
